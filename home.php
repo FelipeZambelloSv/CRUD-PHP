9624236c04c6a79c3309b31052f7841fa19b71ca
FelipeZambelloSv <rm99843@fiap.com.br>
2023-09-04 17:25:07 -0300

    criando home

---
+++ home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+
+<body>
+    <h1>Já tem Cadastro?</h1>
+    <h2>Caso ja tenha, entre em login!!</h2>
+    <div class="box">
+        <a href="./index.php">login</a>
+        <a href="">Cadastre-se</a>
+    </div>
+</body>
+</html>
